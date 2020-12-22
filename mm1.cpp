7dc8e1246f80ce802e71cc9799de7c00e5d490ee
syafiq ali <49745060+bzzrawr@users.noreply.github.com>
2020-12-22 12:03:58 +0800

    Update mm1.cpp

--- mm1.cpp
+++ mm1.cpp
@@ -1,58 +1,37 @@
-// #include<iostream>
-// using namespace std;
-
-// class sim{
-// private:
-// 	double simclock = 0;
-// 	double at = 0
-// 	int salary;
-
-// public:
-// 	void set_dt(double s){
-// 		salary = s;
-// 	}
-// 	int get_dt(){
-// 		return salary;
-// 	}
-// }
-
-// int main(){
-// 	sim test;
-// 	test.set_dt(100.1);
-// 	std::cout<<"test"<<test.get_dt();
-// 	return 0;
-// }
 #include <iostream>
 #include <limits>
+#include <array>
+#include <algorithm>
 // using namespace std;
 
 class Simulation {
   private:
     // Private attribute
     double dt = std::numeric_limits<double>::max();
-    double at = 0;
+    double at = 1.1;
+    double simclock = 0;
+    double et = 0.0;
+    std::array<double,1> sink;
+    int n_arrival = 0;
+    int n_depart = 0;
+    int npdrop = 0;
+    int pid = 0;
+    int maxque = 0;
+
   public:
-    // Setter
-    void setDt(double s) {
-      dt = s;
-    }
-    // Getter
-    int getDt() {
-      return dt;
-    }
-    int getAt(){
-    	return at;
-    }
+  	int scheduling(){
+  		et = std::min(at,dt);
+  	}
+  	void result(){
+  		std::cout<<"event time:"<<et<<std::endl;
+  	}
+
 };
 
 int main() {
-  Simulation myObj;
-  double x = myObj.getDt();
-  double y = myObj.getAt();
-  if(x > y){
-  	myObj.setDt(50000);
-  	std::cout << myObj.getDt();
-  }
-  std::cout << myObj.getDt();
+	Simulation test;
+	test.scheduling();
+	test.result();
+
   
 }
