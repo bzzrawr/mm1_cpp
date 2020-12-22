4a6a19d3af27ce305156a1ce85f3e526a9b87326
syafiq ali <49745060+bzzrawr@users.noreply.github.com>
2020-12-22 19:31:07 +0800

    Update mm1.cpp

--- mm1.cpp
+++ mm1.cpp
@@ -2,36 +2,102 @@
 #include <limits>
 #include <array>
 #include <algorithm>
-// using namespace std;
+#include <random>
+#include <queue>
+
+//  random generator using exponential distribution
+std::default_random_engine generator;
+std::exponential_distribution<double>arrivalrate(6.0);
+std::exponential_distribution<double>servicetime(3.0);
 
 class Simulation {
   private:
     // Private attribute
     double dt = std::numeric_limits<double>::max();
-    double at = 1.1;
+    double at = p_gen();
     double simclock = 0;
     double et = 0.0;
-    std::array<double,1> sink;
+    std::queue<double> sink;
     int n_arrival = 0;
     int n_depart = 0;
     int npdrop = 0;
     int pid = 0;
-    int maxque = 0;
+    int maxque;
+    int cqs = 0;
+    std::queue<double> Myqueue;
+    double tmp1 = s_gen();
+    int etype;
 
   public:
   	void scheduling(){
   		et = std::min(at,dt);
+  		if(cqs < maxque){
+  			pid++;
+  			Myqueue.push(at);
+  			if(sink.size()==0){
+  			    sink.push(Myqueue.front());
+  			    Myqueue.pop();
+  			    etype = 0;
+  			}
+  			else{
+  			    cqs++;
+  			    at = simclock + p_gen();
+  			}
+  		}
+  		else{
+  			npdrop++;
+  			at = simclock + p_gen();
+  		}
+  		std::cout<<"pid:"<<pid<<std::endl;
+  		std::cout<<"Currently in queue:"<<Myqueue.front()<<std::endl;
+  		std::cout<<"Currently in sink:"<<sink.front()<<std::endl;
+  		std::cout<<"arrival time"<<at<<std::endl;
+  	}
+  	void event(){
+  	    if(etype == 0){
+  	        tmp1 = s_gen();
+  	        std::cout<<">>>>Service time>>>>"<<tmp1<<std::endl;
+  	        dt = simclock + tmp1;
+  	    }
+  	}
+
+  	void update_clock(){
+  		simclock = et;
   	}
   	void result(){
-  		std::cout<<"event time:"<<et<<std::endl;
+  		std::cout<<"departure time:"<<dt<<std::endl;
+  	}
+
+  	double p_gen(){
+  		double nRandom = arrivalrate(generator);
+  		return nRandom;
+  	}
+  	double s_gen(){
+  		double nRandom2 = servicetime(generator);
+  		return nRandom2;
+  	}
+  	double get_et(){
+  		return et;
+  	}
+  	
+  	void ggwp(){
+  	    std::cout<<"Enter maximum queue size: ";
+  	    std::cin>>maxque;
   	}
 
 };
 
 int main() {
 	Simulation test;
+	test.ggwp();
+	for(int i=0;i<10;++i){
 	test.scheduling();
+	test.update_clock();
+	test.event();
 	test.result();
+
+	}
+	
 
   
 }
