06cf5d533ccfe114121e4a0e03d210ceda11f36f
syafiq ali <49745060+bzzrawr@users.noreply.github.com>
2020-12-22 09:54:44 +0800

    Update mm1.cpp

--- mm1.cpp
+++ mm1.cpp
@@ -1,24 +1,49 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+
+// class sim{
+// private:
+// 	double simclock = 0;
+// 	double at = 0
+// 	int salary;
+
+// public:
+// 	void set_dt(double s){
+// 		salary = s;
+// 	}
+// 	int get_dt(){
+// 		return salary;
+// 	}
+// }
+
+// int main(){
+// 	sim test;
+// 	test.set_dt(100.1);
+// 	std::cout<<"test"<<test.get_dt();
+// 	return 0;
+// }
+#include <iostream>
 using namespace std;
 
-class sim{
-private:
-	double simclock = 0;
-	double at = 0
-	int salary;
+class Employee {
+  private:
+    // Private attribute
+    int salary;
 
-public:
-	void set_dt(double s){
-		salary = s;
-	}
-	int get_dt(){
-		return salary;
-	}
-}
+  public:
+    // Setter
+    void setSalary(int s) {
+      salary = s;
+    }
+    // Getter
+    int getSalary() {
+      return salary;
+    }
+};
 
-int main(){
-	sim test;
-	test.set_dt(100.1);
-	std::cout<<"test"<<test.get_dt();
-	return 0;
+int main() {
+  Employee myObj;
+  myObj.setSalary(50000);
+  cout << myObj.getSalary();
+  return 0;
 }
