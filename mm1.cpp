be970c3ad6d67b39a327e68ecc0557bc5c045aa4
syafiq ali <49745060+bzzrawr@users.noreply.github.com>
2020-12-22 09:33:42 +0800

    Update mm1.cpp

--- mm1.cpp
+++ mm1.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 
+class sim{
+private:
+	double simclock = 0;
+	double at = 0
+	double dt = std::numeric_limits<double>::infinity();
+
+public:
+	void get_dt(){
+		std::cout<<"DT value"<<dt<<endl;
+	}
+}
+
 int main(){
-	std::cout<<"test";
+	sim test;
+	test.get_dt();
+	return 0;
 }
